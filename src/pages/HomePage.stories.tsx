9ace204ed866d1af66cd28d0900f5475ed76c030
Margo <m.yunanova@gmail.com>
2024-06-23 14:22:01 +0300

    fix: fix HomePage storybook

--- src/pages/HomePage.stories.tsx
+++ src/pages/HomePage.stories.tsx
@@ -10,9 +10,6 @@ import {
   PanelHeader,
   View,
 } from '@vkontakte/vkui';
-import { PopoverWithAllTriggers } from '../widgets/popover-with-all-triggers';
-import { Icon16MoreVertical } from '@vkontakte/icons';
-import song from '../../public/el-gitano-del-amor.mp3';
 
 const meta: Meta<typeof HomePage> = {
   title: 'pages/HomePage',
@@ -41,52 +38,6 @@ const meta: Meta<typeof HomePage> = {
       </ConfigProvider>
     ),
   ],
-  args: {
-    songs: [
-      {
-        title: 'Back in Black',
-        artist: 'AC/DC',
-        cover:
-          'https://avatars.dzeninfra.ru/get-zen_doc/2408175/pub_5ea838828e079d5083ec827c_5ea85d5b9e714f47e1bfe961/scale_2400',
-        // time: '3:23',
-        button: (
-          <PopoverWithAllTriggers
-            content={['Скопировать ссылку', 'Поделится', 'Не нравится']}
-            button={<Icon16MoreVertical />}
-          />
-        ),
-        song,
-      },
-      {
-        title: 'Back in Black',
-        artist: 'AC/DC',
-        cover:
-          'https://avatars.dzeninfra.ru/get-zen_doc/2408175/pub_5ea838828e079d5083ec827c_5ea85d5b9e714f47e1bfe961/scale_2400',
-        // time: '3:23',
-        button: (
-          <PopoverWithAllTriggers
-            content={['Скопировать ссылку', 'Поделится', 'Не нравится']}
-            button={<Icon16MoreVertical />}
-          />
-        ),
-        song,
-      },
-      {
-        title: 'Back in Black',
-        artist: 'AC/DC',
-        cover:
-          'https://avatars.dzeninfra.ru/get-zen_doc/2408175/pub_5ea838828e079d5083ec827c_5ea85d5b9e714f47e1bfe961/scale_2400',
-        // time: '3:23',
-        button: (
-          <PopoverWithAllTriggers
-            content={['Скопировать ссылку', 'Поделится', 'Не нравится']}
-            button={<Icon16MoreVertical />}
-          />
-        ),
-        song,
-      },
-    ],
-  },
 } satisfies Meta<typeof HomePage>;
 
 export default meta;
